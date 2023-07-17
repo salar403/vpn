5665c077c500070af11697154a56441404528d3b
salar karbalai <salar40340@gmail.com>
2023-07-17 13:29:01 +0330

    shellscript bug fixed

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 CERT_PATH="./ssl"
 mkdir $CERT_PATH
-openssl req -new -newkey rsa:4096 -days 3600 -nodes -x509 -subj '/C=IR/ST=Tehran/' -keyout $CERT_PATH/cert.pem -out $CERT_PATH/key.pem
+openssl req -new -newkey rsa:4096 -days 3600 -nodes -x509 -subj '/C=IR/ST=Tehran/' -keyout $CERT_PATH/key.pem -out $CERT_PATH/cert.pem
