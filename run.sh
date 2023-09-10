8063e914e3ed753724795c429504fd14a41ae36e
salar karbalai <salar40340@gmail.com>
2023-09-10 12:46:08 +0330

    added missing docker command

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 CERT_PATH="./ssl"
 mkdir $CERT_PATH
 openssl req -new -newkey rsa:4096 -days 3600 -nodes -x509 -subj '/C=IR/ST=Tehran/' -keyout $CERT_PATH/key.pem -out $CERT_PATH/cert.pem
+docker network create v2ray-network
